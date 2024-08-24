cb4b45d399cd48df2340a73739c6e76c83ab6ce6
Ashish Walia <waliaashu1998@gmail.com>
2024-08-24 19:11:06 +0530

    First Commit

---
+++ src/Day_1/dataTypesAndVariables.java
@@ -0,0 +1,36 @@
+package Day_1;
+
+public class dataTypesAndVariables {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // variables and datatypes +
+        int num = 255555555; //store a range of data ---> max size ==>>  4 bytes
+        short shortNum = 9999; // 2bytes
+        byte byteNum = 99; //1 byte
+        long longNum = 999999999; //8bytes
+
+        /**
+         *Decimal Data Type
+         */
+
+        float fNum = 123.12312312312f;
+        double dNum =   123.123131231231312321213;
+
+        /**
+         *
+         * Boolean Data Types
+         */
+
+        Boolean isReady  = true; // 1 bit
+        isReady = false;
+
+        /**
+         *
+         * Character Data type
+         */
+        char ch = 'a';  //2 bytes
+
+        // Var is add on java in java 10. it's is dynamically
+    }
+}
