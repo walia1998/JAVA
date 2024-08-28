a00459ec50c3369253a3f0e34138aff56c608a90
Ashish Walia <waliaashu1998@gmail.com>
2024-08-28 19:16:15 +0530

    today commit

--- src/JavaConditions/conditions.java
+++ src/JavaConditions/conditions.java
@@ -2,13 +2,26 @@ package JavaConditions;
 
 public class conditions {
     public static void main(String[] args) {
-        boolean sheAgreed = true;
+        //boolean sheAgreed = true;
 
-        if(sheAgreed) {
-            System.out.println("Good");
+//        if(sheAgreed) {
+//            System.out.println("Good");
+//        }else {
+//            System.out.println("Hare Krishan");
+//        }
+//        System.out.println("Omly Krishna");
+
+        int a = 20;
+        int b = 30;
+        int c = 40;
+
+        if(a > b && b > c ){
+            System.out.println("A is greater");
+        } else if (b > a && b > c) {
+            System.out.println("B is greater ");
         }else {
-            System.out.println("Hare Krishan");
+            System.out.println("C is greater");
         }
-        System.out.println("Omly Krishna");
+
     }
 }
